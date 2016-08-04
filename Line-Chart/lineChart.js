2bad68d371f2f88ef5fb2652a7241732f15be020
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-08-05 01:03:42 +0200

    Line Chart

--- Line-Chart/lineChart.js
+++ Line-Chart/lineChart.js
@@ -1,11 +1,30 @@
 window.onload = function() {
     Flotr.draw(
         document.getElementById("chart"), [{
+            data: zero,
+            label: "20<sup>th</sup>-Century Baseline Temperature",
+            lines: {
+                show: true,
+                lineWidth: 1
+            },
+            yaxis: 2,
+            shadowSize: 0,
+            color: "red"
+        }, {
             data: co2,
+            label: "CO<sub>2</sub>-Concentration(ppm)",
             lines: {
                 show: true
             }
+        }, {
+            data: temp,
+            label: "Yearly Temperature Difference (°C)",
+            lines: {
+                show: true
+            },
+            yaxis: 2
         }], {
+            title: "Global Temperature and CO<sub>2</sub> Concentration (NOAA Data)",
             grid: {
                 horizontalLines: false,
                 verticalLines: false
@@ -13,6 +32,14 @@ window.onload = function() {
             yaxis: {
                 min: 300,
                 max: 400
+            },
+
+            yaxis2: {
+                min: -0.15,
+                max: 0.69,
+                tickFormatter: function(val) {
+                    return val + " °C";
+                }
             }
         }
     );
